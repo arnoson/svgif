4f372b611a9dfb3648415aa9906b304c7478728e
arnoson <hallo@arno-schlipf.info>
2024-11-20 16:48:16 +0100

    fix: use correct base path

--- vite.config.ts
+++ vite.config.ts
@@ -4,13 +4,10 @@ import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
 // https://vite.dev/config/
-export default defineConfig({
-  plugins: [
-    vue(),
-  ],
+export default defineConfig(({ mode }) => ({
+  plugins: [vue()],
+  base: mode === 'development' ? '/' : '/svgif/',
   resolve: {
-    alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
-    },
+    alias: { '@': fileURLToPath(new URL('./src', import.meta.url)) },
   },
-})
+}))
